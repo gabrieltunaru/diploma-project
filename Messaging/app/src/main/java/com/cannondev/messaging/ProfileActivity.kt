efed1900dd64437d59e10ddd7040447028ef3ffc
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2021-04-18 20:46:24 +0300

    started adding navigation

--- Messaging/app/src/main/java/com/cannondev/messaging/ProfileActivity.kt
+++ Messaging/app/src/main/java/com/cannondev/messaging/ProfileActivity.kt
@@ -3,16 +3,25 @@ package com.cannondev.messaging
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.android.volley.Response
 import com.cannondev.messaging.http.Queue
 import com.cannondev.messaging.http.UserHttp
 import com.cannondev.messaging.models.ProfileModel
 import com.cannondev.messaging.models.UserModel
 import com.cannondev.messaging.utils.ImageHandler
+import com.google.android.material.navigation.NavigationView
 import com.google.gson.Gson
 
 
@@ -25,6 +34,8 @@ class ProfileActivity : AppCompatActivity() {
     lateinit var photo: ImageView
     lateinit var user: UserModel
     lateinit var fileName: String
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     fun getCurrentUser() {
         val req = userHttp.getCurrentUser(applicationContext, Response.Listener { data ->
             user = Gson().fromJson(data, UserModel::class.java)
@@ -69,7 +80,21 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
         username = findViewById(R.id.profileUsername)
         details = findViewById(R.id.profileDetails)
         photo = findViewById(R.id.profileAvatar)
@@ -80,6 +105,17 @@ class ProfileActivity : AppCompatActivity() {
         authToken = sharedPref.getString("authToken", "").toString()
         userHttp = UserHttp(authToken)
         getCurrentUser()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.navigation, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
